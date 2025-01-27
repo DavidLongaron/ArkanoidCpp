2e817f6169a4a31746d990c98897026934520d73
David <dlongaron@hotmail.com>
2025-01-27 22:55:13 +0100

    sdl

--- ArkanoidC++/ArkanoidC++.cpp
+++ ArkanoidC++/ArkanoidC++.cpp
@@ -1,11 +1,47 @@
-// ArkanoidC++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <SDL3/SDL.h>
 #include <iostream>
 
-int main()
-{
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "Error al inicializar SDL3: " << SDL_GetError() << std::endl;
+        return -1;
+    }
 
+    SDL_Window* window = SDL_CreateWindow("Ventana SDL3", 800, 600,0);
 
-    std::cout << "Hello World!\n";
+    if (!window) {
+        std::cerr << "Error al crear la ventana: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+
+    if (!renderer) {
+        std::cerr << "Error al crear el renderer: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    bool isRunning = true;
+    SDL_Event event;
+
+    while (isRunning) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                isRunning = false;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // Azul
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
 }
